97b35c2d19eaac6ee461d1323be8867ec628c844
Manuel Rivas Gauna <manuelrivasgauna@cofarsur.com>
2023-06-30 15:10:57 -0300

    Modifique JWtInterceptor para obtener el token directamente de authorization-Bearertoken

--- api/src/main/java/com/example/api/config/JwtInterceptor.java
+++ api/src/main/java/com/example/api/config/JwtInterceptor.java
@@ -1,8 +1,8 @@
 package com.example.api.config;
 
+import com.example.api.common.AccessDeniedException;
 import com.example.api.dto.RequestMeta;
 import com.example.api.utils.JwtUtils;
-import com.example.api.utils.TokenHolder;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,19 +20,25 @@ public class JwtInterceptor implements HandlerInterceptor {
     public JwtInterceptor (RequestMeta requestMeta){
         this.requestMeta = requestMeta;
     }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            String auth = request.getHeader("Authorization");
+            //Exceptua esas rutas para que no verifiquen el token
+            if (!(request.getRequestURI().contains("login") || request.getRequestURI().contains("signup"))) {
+                // Verifica si el encabezado de autorización está presente y tiene el formato adecuado
+                if (auth != null && auth.startsWith("Bearer ")) {
+                    String token = auth.substring(7); // Elimina el prefijo "Bearer " para obtener el token
 
-        String auth = TokenHolder.accessToken;
+                    Claims claims = jwtUtils.verify(token);
 
-        //Exceptua esas rutas para que no verifiquen el token
-        if (!(request.getRequestURI().contains("login") || request.getRequestURI().contains("signup"))){
-            Claims claims = jwtUtils.verify(auth);
-
-            requestMeta.setUsername(claims.get("username").toString());
-            requestMeta.setUserId(Long.valueOf(claims.getIssuer()));
-        }
-
+                    requestMeta.setUsername(claims.get("username").toString());
+                    requestMeta.setUserId(Long.valueOf(claims.getIssuer()));
+                } else {
+                    // Lanza una excepción si el token de autorización no está presente o no tiene el formato adecuado
+                    throw new AccessDeniedException("Acceso denegado");
+                }
+            }
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 }
