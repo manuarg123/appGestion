de6edd5e7dae921447d9ce5e9bb82243737620ae
Manuel Rivas Gauna <manuelrivasgauna@cofarsur.com>
2023-07-27 15:15:20 -0300

    Ejemplo basico consumir api y listar

--- angularFront/src/app/components/home/home.component.ts
+++ angularFront/src/app/components/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  data : any = {};
 
+  constructor(private apiService:ApiService){}
+
+  ngOnInit(): void {
+    this.llenar();
+  }
+
+  llenar(){
+    this.apiService.getData().subscribe( data => {
+      this.data = data;
+      console.log(this.data);
+    })
+  }
 }
--- angularFront/src/app/service/api.service.ts
+++ angularFront/src/app/service/api.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private urlAPI = "https://apisimpsons.fly.dev/api/personajes";
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public getData() : Observable<any> {
+    return this.http.get<any>(this.urlAPI);
+  }
 }
